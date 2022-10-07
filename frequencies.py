1e5ed3e7864f7e9a3cf1a013e8ffeb0a44fb5599
Reshma <reshma.dhillon@kcl.ac.uk>
2022-10-07 12:43:22 +0100

    Fixed string conversion

--- frequencies.py
+++ frequencies.py
@@ -5,9 +5,17 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     for letter in items:
-        if frequencies.get(letter):
-            newFrequency = frequencies.get(letter) + 1
-            frequencies.update({f'{letter}':newFrequency})
+        strLetter = f'{letter}'
+        if frequencies.get(strLetter):
+            print(letter)
+            newFrequency = frequencies.get(strLetter) + 1
+            frequencies.update({strLetter:newFrequency})
+            print(frequencies)
         else:
-            frequencies.update({f'{letter}':1})
+            print(letter)
+            frequencies.update({strLetter:1})
+            print(frequencies)
+    print(frequencies)
     return frequencies
+
+frequencies(['0', 4,4,'4','d','d','e',0,'a','d','4'])
