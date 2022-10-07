9b346286d72e46307b93e35fc11708c7652722e0
Reshma <reshma.dhillon@kcl.ac.uk>
2022-10-07 12:45:50 +0100

    Removed test

--- frequencies.py
+++ frequencies.py
@@ -3,7 +3,6 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
     for letter in items:
         strLetter = f'{letter}'
         if frequencies.get(strLetter):
@@ -17,5 +16,3 @@ def frequencies(items):
             print(frequencies)
     print(frequencies)
     return frequencies
-
-frequencies(['0', 4,4,'4','d','d','e',0,'a','d','4'])
