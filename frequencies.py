1de9609e4234507549117f43e56cbcc1ce959334
Reshma <reshma.dhillon@kcl.ac.uk>
2022-10-07 12:35:36 +0100

    1st attempt

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for letter in items:
+        if frequencies.get(letter):
+            newFrequency = frequencies.get(letter) + 1
+            frequencies.update({f'{letter}':newFrequency})
+        else:
+            frequencies.update({f'{letter}':1})
     return frequencies
